7400c2641df06b159a8e0e2089990af823b730c1
执生 <144300052+zapsaang@users.noreply.github.com>
2023-09-15 11:44:02 +0800

    Update general.zsh

--- plugins/general.zsh
+++ plugins/general.zsh
@@ -1,5 +1,13 @@
-alias ll='ls -ahlF'
-alias la='ls -A'
-alias l='ls -CF'
+export LS_OPTINS='--color=auto'
+
+alias ll='ls $LS_OPTINS -ahlF'
+alias la='ls $LS_OPTINS -A'
+alias ls='ls $LS_OPTINS'
+alias l='ls $LS_OPTINS -CF'
+
 alias pip='pip3'
 alias python='python3'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
