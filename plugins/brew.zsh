4b121d609295ac2a60ca176d63ac641334eaa0b9
执生 <144300052+zapsaang@users.noreply.github.com>
2024-04-27 10:57:46 +0800

    Update brew.zsh

--- plugins/brew.zsh
+++ plugins/brew.zsh
@@ -7,4 +7,12 @@
     export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}"
 }
 
+[[ -s "/opt/homebrew/bin/brew" && $(which brew) == "brew not found" ]] && {
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+    export HOMEBREW_REPOSITORY="/opt/homebrew";
+    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+    export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+}
+
 alias bud="brew update && brew upgrade"
