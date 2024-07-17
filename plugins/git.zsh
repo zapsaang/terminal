2f4eadc652bceac3f7860f90766b32cdb7543ecf
执生 <144300052+zapsaang@users.noreply.github.com>
2024-07-17 15:15:10 +0800

    Update git.zsh

--- plugins/git.zsh
+++ plugins/git.zsh
@@ -1,1 +1,7 @@
-alias grcp='git add . && git commit -m "resolve conflict" && git push'
+function grcp() {
+  git add . && git commit -m "resolve conflict" && git push
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  if [[ "$current_branch" == "conflict"* ]]; then
+    git checkout -
+  fi
+}
