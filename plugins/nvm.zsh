54b9ba0751725bb4b480548ba5c2dfe22264fa4e
执生 <144300052+zapsaang@users.noreply.github.com>
2025-03-23 20:22:57 +0800

    Update nvm.zsh

--- plugins/nvm.zsh
+++ plugins/nvm.zsh
@@ -2,4 +2,9 @@ function nvmup() {
   export NVM_DIR="$HOME/.nvm"
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+  if [[ -n "$1" ]]; then
+    nvm use "$1"
+  else
+    nvm use --lts || (nvm install --lts && nvm use --lts)
+  fi
 }
