4d9e84c67cbdf771df52d81fde8160ab0be6cf64
执生 <144300052+zapsaang@users.noreply.github.com>
2025-08-27 20:40:22 +0800

    Update general.zsh

--- plugins/general.zsh
+++ plugins/general.zsh
@@ -1,26 +1,26 @@
 export LS_OPTIONS='--color=auto'
 
-alias ll='ls $LS_OPTIONS -ahlF'
-alias la='ls $LS_OPTIONS -A'
-alias ls='ls $LS_OPTIONS'
-alias l='ls $LS_OPTIONS -CF'
+alias ll="ls $LS_OPTIONS -ahlF"
+alias la="ls $LS_OPTIONS -A"
+alias ls="ls $LS_OPTIONS"
+alias l="ls $LS_OPTIONS -CF"
 
-alias pip='pip3'
-alias python='python3'
+alias pip="pip3"
+alias python="python3"
 
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
 
-alias sudo='sudo -i'
+alias sudo="sudo -i"
 
-alias df='df -h'
-alias du='du -h'
+alias df="df -h"
+alias du="du -h"
 
-function bddc() {
+bddc() {
     echo "$1" | base64 -Dd | zstd -d -o "$2"
 }
 
-function bdsn() {
+bdsn() {
     echo "$1" | base64 -Dd | snzip -dc > "$2"
 }
