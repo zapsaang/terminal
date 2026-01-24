d275665cd8afc506f4b4a8b87f90de1a8543dfa6
执生 <144300052+zapsaang@users.noreply.github.com>
2026-01-24 17:06:51 +0800

    Update brew.zsh

--- plugins/brew.zsh
+++ plugins/brew.zsh
@@ -1,7 +1,7 @@
 for brew_path in "/opt/homebrew" "/home/linuxbrew/.linuxbrew"; do
     brew_bin="$brew_path/bin"
     brew_sbin="$brew_path/sbin"
-    if [[ -s "$brew_path/bin/brew" ]] && (( ! ${path[(I)(#q)$brew_sbin]} )); then
+    if [[ -x "$brew_path/bin/brew" ]] && (( ! ${path[(I)(#q)$brew_sbin]} )); then
         export HOMEBREW_PREFIX="$brew_path"
         export HOMEBREW_CELLAR="$brew_path/Cellar"
         export HOMEBREW_REPOSITORY="$brew_path"
